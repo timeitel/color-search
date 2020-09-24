d11486618359f89bbac6367d6425cb5ee0e36eac
Tim Eitel <timeitel@outlook.com>
2020-09-25 07:46:57 +0800

    style: wrap table and scroll overflow

--- components/Table/table.js
+++ components/Table/table.js
@@ -4,24 +4,30 @@ const table = ({ colors }) => {
   const rows = colors.map(({ color, hex }) => {
     return (
       <tr key={hex}>
+        <td></td>
         <td>{color}</td>
         <td>{hex}</td>
+        <td></td>
+        <td></td>
       </tr>
     );
   });
 
   return (
-    <table className={styles.table}>
-      <tbody>
-        <tr>
-          <th>Name</th>
-          <th>Hex</th>
-          <th>RGB</th>
-          <th>CMYK</th>
-        </tr>
-        {rows}
-      </tbody>
-    </table>
+    <div className={styles.container}>
+      <table className={styles.table}>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Hex</th>
+            <th>RGB</th>
+            <th>CMYK</th>
+          </tr>
+          {rows}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
