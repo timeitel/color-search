57bf8afab9d762e88704cc964f9f2b8d9bbc6e42
Tim Eitel <timeitel@outlook.com>
2020-09-25 09:20:49 +0800

    update: repo link

--- components/ColorSearch/colorsearch.js
+++ components/ColorSearch/colorsearch.js
@@ -57,7 +57,21 @@ const colorsearch = ({ colors }) => {
         target='_blank'
         style={repoStyle}
       >
-        <img src='/github.png' />
+        Github Repo
+        <svg
+          fill='none'
+          stroke='currentColor'
+          viewBox='0 0 24 24'
+          xmlns='http://www.w3.org/2000/svg'
+          height='16'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth='2'
+            d='M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14'
+          ></path>
+        </svg>
       </a>
       <input
         onKeyDown={handleSearch}
